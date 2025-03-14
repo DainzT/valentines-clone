5d4832f5b5138fc83949984792a6e40541b1f1e5
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-03-14 22:48:01 +0800

    Create Components & Pages

---
+++ activity 13-03-2025/src/components/EmployeeTable.tsx
@@ -0,0 +1,28 @@
+const EmployeeTable = () => {
+    return (
+        <>
+            <tr>
+                <td>
+                    First Name
+                </td>
+                <td>
+                    Last Name
+                </td>
+                <td>
+                    Group Name
+                </td>
+                <td>
+                    Role
+                </td>
+                <td>
+                    Expected Salary
+                </td>
+                <td>
+                    Expected Date of Defense
+                </td>
+            </tr>
+        </>
+    );
+};
+
+export default EmployeeTable;
---
+++ activity 13-03-2025/src/pages/EmployeeManagementSystemPage.tsx
@@ -0,0 +1,11 @@
+import EmployeeTable from "../components/EmployeeTable";
+
+const EmployeeManagementSystemPage = () => {
+    return (
+        <>
+            <EmployeeTable/>
+        </>
+    );
+};
+
+export default EmployeeManagementSystemPage;
